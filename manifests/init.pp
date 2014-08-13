08bdc5f8f677602e8a577d0569ec145a34e55dd5
Joe Rodriguez Jr <jrod@diggalabs.com>
2014-08-13 11:50:23 -0500

    Fixing manifest.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,19 @@
+# == Class: apticron
+#
+# Installs package apticron
+#
+# === Parameters
+#
+# === Authors
+#
+# SysOps <sysops@diggalabs.com>
+#
+# === Copyright
+#
+# Copyright 2014 DiggaLabs Inc., unless otherwise noted.
+#
 class apticron {
- package {'apticron':
+  package { 'apticron':
   ensure  => 'latest',
- }
+  }
 }
